804997dc0ba2cfc5889f61074f711000d005e77f
deepak.ghadge <deepak.ghadge@in.ncs-i.com>
2023-06-14 21:38:27 +0530

    Spring Project

---
+++ UserRestfulDemo/src/main/java/com/rest/UserRestfulDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRestfulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRestfulDemoApplication.class, args);
+	}
+
+}
